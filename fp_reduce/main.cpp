7fc89a30560109a1f269bca4179baac18618b0c2
Juhnowski <juhnowski@gmail.com>
2020-08-01 05:17:49 +0300

    Initial Commit

---
+++ fp_reduce/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fp_reduce
+//
+//  Created by Илья Юхновский on 01.08.2020.
+//  Copyright © 2020 Илья Юхновский. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
